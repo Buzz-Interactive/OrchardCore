b41b701f072c2a4aab7467cb0ee8da6287c2d483
Nick Jackson <nick@buzzinteractive.co.uk>
2025-12-11 16:01:48 +0000

    feat: Initial OrchardCore.AdminBar setup
    Added an empty project for OrchardCore.AdminBar and added reference to OrchardCore.Application.Cms.Core.Targets.csproj

---
+++ src/OrchardCore.Modules/OrchardCore.AdminBar/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "Admin Bar",
+    Author = ManifestConstants.OrchardCoreTeam,
+    Website = ManifestConstants.OrchardCoreWebsite,
+    Version = ManifestConstants.OrchardCoreVersion,
+    Description = "Provides admin bar functionality.",
+    Category = "Admin"
+)]
